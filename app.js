55fd9978c66c3e27fef8805419809e8bdd0a1c26
geomaka <107925640+geomaka@users.noreply.github.com>
2023-04-07 17:07:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const { result } = require('lodash');
 const blogRoutes = require('./routes/blogRoutes.js')
-//exprss app
+
+//express app
 const  app = express();
 const mongoURL ="mongodb+srv://geo:totoivy@cluster0.bpodhqs.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(mongoURL)
@@ -22,66 +22,14 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(morgan('dev'));
 
-// mongoose and mongo sandbox routes
-
-// app.get('/add-blog', (req,res) =>{
-//     const blog = new Blog({
-//         title : "new blog",
-//         snippet : "about my new blog",
-//         body : "more about my blog"
-//     });
-//     blog.save()
-//     .then((result) =>{
-//         res.send(result)
-//     })
-//     .catch((err) =>{
-//         console.log(err)
-//     })
-// })
-// app.get('/all-blogs',(res,req) =>{
-//     Blog.find()
-//     .then((result) =>{
-//         res.send(result)
-//     })
-//     .catch((err) =>{
-//         console.log(err)
-//     })
-// })
-
-// app.get('/single-blog',(req,res) =>{
-//     Blog.findById('641334e6eeb66c479cf577c4')
-//     .then((result) =>{
-//         res.send(result)
-//     })
-//     .catch((err) =>{
-//         console.log(err)
-//     })
-// })
-
-
 app.get('/',(req,res) => {
-//     const blogs = [
-//         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-//         {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-//         {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-//       ];
-//    // res.send('<p>home page</p>');
-//    res.render('index',{title:'home',blogs});
 res.redirect('/blogs')
 })
 app.get('/about',(req,res) => {
-   // res.send('<p>about page</p>');
    res.render('about',{title:'about'})
 })
 //blog routes
 app.use('/blogs',blogRoutes)
-
-// //redirects
-// app.get('/about-us',(req,res) =>{
-//     res.redirect('about');
-// })
-
-
 
 //404 page
 app.use((req,res) =>{
